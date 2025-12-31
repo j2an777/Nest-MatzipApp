eb9dd74dfd113df32eae9e3eada195f2a3985b58
j2an777 <ha991104@gmail.com>
2025-12-31 15:26:08 +0900

    fix: auth module도 마찬가지로 해당 엔티티 사용을 위해 imports 속성 추가 적용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { User } from './auth.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
