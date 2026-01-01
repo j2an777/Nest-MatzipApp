8aca1c159da1c7c9ae6c8fb964ea70fbfef601f4
j2an777 <ha991104@gmail.com>
2026-01-01 17:45:15 +0900

    feat: 토큰 재발급을 위한 refresh 컨트롤러 생성
    커스텀 데코레이터에서 유저 정보를 가져오기 위해서는 AuthGuard를 넣어준다.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import { GetUser } from '@common/decorators/get-user.decorator';
 
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { User } from './auth.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +26,11 @@ export class AuthController {
   @Post('/signin')
   signin(@Body(ValidationPipe) authDto: AuthDto) {
     return this.authService.signin(authDto);
+  }
+
+  @Get('/refresh')
+  @UseGuards(AuthGuard())
+  getNewToken(@GetUser() user: User) {
+    return this.authService.getNewToken(user);
   }
 }
