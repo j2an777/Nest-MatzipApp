0af6c3c5426dd8cc91fef901f08b13cd78228ca6
j2an777 <ha991104@gmail.com>
2026-01-02 21:52:31 +0900

    feat: 상위 모듈에서는 Image 엔티티 사용과 AuthModule 사용을 위해 imports 구성

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,7 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from '@/auth/auth.module';
+
 import { ImageController } from './image.controller';
+import { Image } from './image.entity';
 
 @Module({
-  controllers: [ImageController]
+  imports: [TypeOrmModule.forFeature([Image]), AuthModule],
+  controllers: [ImageController],
 })
 export class ImageModule {}
