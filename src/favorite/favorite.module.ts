9ff3f6f8c466b902e3317059d86a1150002b5242
j2an777 <ha991104@gmail.com>
2026-01-02 23:25:54 +0900

    feat: favorite entity와 AuthModule 사용가능하게 하기 위해 imports 설정

--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -1,9 +1,15 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from '@/auth/auth.module';
+
 import { FavoriteController } from './favorite.controller';
 import { FavoriteService } from './favorite.service';
+import { Favorite } from './favorite.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Favorite]), AuthModule],
   controllers: [FavoriteController],
-  providers: [FavoriteService]
+  providers: [FavoriteService],
 })
 export class FavoriteModule {}
