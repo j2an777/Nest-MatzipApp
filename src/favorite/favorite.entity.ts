0710fe7579c2053cfac266071e6a264fd00ce46d
j2an777 <ha991104@gmail.com>
2026-01-02 23:24:42 +0900

    feat: 즐겨찾기 엔티티 구성
    post, user의 경우 해당 즐겨찾기 조회시 필요하기 때문에 join으로 각 id값을 기준으로 가져오도록 하고, CASCADE로 부모 칼럼이 사라지면 같이 사라지도록 설정

---
+++ src/favorite/favorite.entity.ts
@@ -0,0 +1,43 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { User } from '@/auth/user.entity';
+import { Post } from '@/post/post.entity';
+
+@Entity()
+export class Favorite extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  postId: number;
+
+  @Column()
+  userId: number;
+
+  @ManyToOne(() => Post, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'postId' })
+  post: Post;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+}
