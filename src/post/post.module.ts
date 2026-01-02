f490871a8190024f1d83ab6a73bd794a54a0791d
j2an777 <ha991104@gmail.com>
2026-01-02 22:16:11 +0900

    feat: post 모듈에서도 Image 엔티티 사용을 위해 forFeature에 추가

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,13 +2,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from '@/auth/auth.module';
+import { Image } from '@/image/image.entity';
 
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 import { Post } from './post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Post, Image]), AuthModule],
   controllers: [PostController],
   providers: [PostService],
 })
