69b14f7c604bfa373a831eaf213d6c162aa9cb37
j2an777 <ha991104@gmail.com>
2026-01-02 21:52:01 +0900

    feat: 이미지 업로드 컨트롤러 생성
    생성 전에 Interceptor로 이미지 개수 와 사이즈 제한 및 스토리지에 담겨질 저장소 및 파일 이름 정의

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { AuthGuard } from '@nestjs/passport';
+import { basename, extname } from 'path';
+import { diskStorage } from 'multer';
+import * as fs from 'fs';
+
+import numbers from '@/@common/constants';
+
+try {
+  fs.readdirSync('uploads');
+} catch (error) {
+  fs.mkdirSync('uploads');
+}
 
 @Controller('image')
-export class ImageController {}
+@UseGuards(AuthGuard())
+export class ImageController {
+  @UseInterceptors(
+    FilesInterceptor('images', numbers.MAX_IMAGE_COUNT, {
+      storage: diskStorage({
+        destination(req, file, cb) {
+          cb(null, 'uploads/');
+        },
+        filename(req, file, cb) {
+          const ext = extname(file.originalname);
+          cb(null, basename(file.originalname, ext) + Date.now() + ext);
+        },
+      }),
+      limits: { fileSize: numbers.MAX_IMAGE_SIZE }, //20MB
+    }),
+  )
+  @Post('/')
+  uploadImages(@UploadedFiles() files: Express.Multer.File[]) {
+    const uris = files.map((file) => file.filename);
+
+    return uris;
+  }
+}
