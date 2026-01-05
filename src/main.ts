40dea50b306212670c612f07b8e12a88d83959fa
j2an777 <ha991104@gmail.com>
2026-01-05 10:28:15 +0900

    fix: bootstrap port값 NODE_ENV 레벨 값에 따른 CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3030);
-  console.log(`http://localhost:${3030}`);
+  const PORT = process.env.PORT || 3030;
+
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: ['https://...'],
+      credentials: true,
+    });
+  } else {
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+  }
+
+  await app.listen(PORT);
 }
 bootstrap();
